c7537a4c4ee76a9c048a155b8f1f8c23917585ed
mitchdzugan <mitchdzugan@gmail.com>
2022-08-28 05:00:10 -0500

    optic helper is working

--- rewriter/ppx_ibad.ml
+++ rewriter/ppx_ibad.ml
@@ -2,14 +2,14 @@ open Ppxlib
 
 let name = "ibad"
 
-let expand ~loc ~path:_ (env : string) =
-  [%expr Some ([%e Ast_builder.Default.estring ("xD " ^ env) ~loc])]
+let expand ~loc ~path:_ optic =
+  [%expr (module struct include (val [%e optic]) end)]
 
 let ext =
   Extension.declare
     name
     Extension.Context.expression
-    Ast_pattern.(single_expr_payload (estring __))
+    Ast_pattern.(single_expr_payload __)
     expand
 
 let () = Ppxlib.Driver.register_transformation name ~extensions:[ext]
